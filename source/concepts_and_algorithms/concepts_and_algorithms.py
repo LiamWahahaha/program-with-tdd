26c83f2e0fac3ca006e4d3413fc92ad1af077d25
Liam <liliwang@cs.stonybrook.edu>
2019-02-09 00:09:35 -0500

    Add an example of dynamic programming - ugly numbers

--- source/concepts_and_algorithms/concepts_and_algorithms.py
+++ source/concepts_and_algorithms/concepts_and_algorithms.py
@@ -40,3 +40,59 @@ class DP():
             hop[idx] = hop[idx - 1] + hop[idx - 2] + hop[idx - 3]
 
         return hop[n]
+
+
+    def ugly_number(self, n):
+        """
+        Ugly numbers are numbers whose only prime factors are 2, 3 or 5.
+        """
+        i2 = 0
+        i3 = 0
+        i5 = 0
+        ugly = [1]
+        next_multiple_of_2 = ugly[i2] * 2
+        next_multiple_of_3 = ugly[i3] * 3
+        next_multiple_of_5 = ugly[i5] * 5
+
+        for idx in range(1, n):
+            ugly.append(min(next_multiple_of_2, next_multiple_of_3, next_multiple_of_5))
+            if ugly[-1] == next_multiple_of_2:
+                i2 += 1
+                next_multiple_of_2 = ugly[i2] * 2
+            if ugly[-1] == next_multiple_of_3:
+                i3 += 1
+                next_multiple_of_3 = ugly[i3] * 3
+            if ugly[-1] == next_multiple_of_5:
+                i5 += 1
+                next_multiple_of_5 = ugly[i5] * 5
+
+        return ugly[-1]
+
+class Sort():
+    """
+    common sorting algorithms
+    """
+
+#    def bubble_sort(self, arr):
+#        len_of_arr = len(arr)
+#
+#        for idx_i in range(len_of_arr):
+#            for idx_j in range(idx_i + 1, len_of_arr):
+#                if arr[idx_i] > arr[idx_j]:
+#                    arr[idx_i], arr[idx_j] = arr[idx_j], arr[idx_i]
+#
+#        return arr
+
+#    def selection_sort(self, arr):
+#        len_of_arr = len(arr)
+#
+#        for idx_i, num in enumerate(arr):
+#            minimum = idx_i
+#            for idx_j in range(idx_i + 1, len_of_arr):
+#                if arr[idx_j] < arr[idx_i]:
+#                    minimum = idx_j
+#
+#            arr[idx_i], arr[minimum] = arr[minimum], arr[idx_i]
+#
+#        return arr
+
--- tests/test_concepts_and_algorithms.py
+++ tests/test_concepts_and_algorithms.py
@@ -11,6 +11,12 @@ class BasicTestSuite(unittest.TestCase):
             self.assertEqual(example.triple_step(3), 4)
             self.assertEqual(example.triple_step(4), 7)
 
+    def test_ugly_number(self):
+        dp = Concepts.DP()
+        self.assertEqual(dp.ugly_number(7), 8)
+        self.assertEqual(dp.ugly_number(10), 12)
+        self.assertEqual(dp.ugly_number(15), 24)
+        self.assertEqual(dp.ugly_number(150), 5832)
 
 if __name__ == '__main__':
     unittest.main()
